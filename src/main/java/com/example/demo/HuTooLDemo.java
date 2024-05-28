1f833bbdf69c7c84e476bd85fe3a7acac3c27f4c
masonFei <994085848@qq.com>
2024-05-28 23:53:21 +0800

    hutool core demo

--- src/main/java/com/example/demo/HuTooLDemo.java
+++ src/main/java/com/example/demo/HuTooLDemo.java
@@ -6,6 +6,7 @@ package com.example.demo;
 
 import cn.hutool.core.convert.Convert;
 import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.lang.Console;
 
 /**
  * HuTooLDemi
@@ -21,6 +22,7 @@ public class HuTooLDemo {
 
 
         String unicode = Convert.strToUnicode("字符串");
+        Console.log(unicode);
 
         int second = DateUtil.timeToSecond("08:30");
 
@@ -28,8 +30,11 @@ public class HuTooLDemo {
 
         System.out.println(second);
 
-
         System.out.println(time);
+        String[] a = {"abc", "bcd"};
+
+        Console.log(a);
+
 
     }
 
