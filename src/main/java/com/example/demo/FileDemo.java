3f5c127d0d99d79c2c88a7f5460980d93eb13e95
feixuanyu <994085848@qq.com>
2025-07-21 10:18:41 +0800

    代码暂存

---
+++ src/main/java/com/example/demo/FileDemo.java
@@ -0,0 +1,30 @@
+/**
+ * LY.com Inc.
+ * Copyright (c) 2004-2025 All Rights Reserved.
+ */
+package com.example.demo;
+
+import java.io.File;
+
+/**
+ * FileDemo
+ *
+ * @author johnny.fei
+ * @version 1.0.0
+ * @since 2025-07-18 10:51
+ */
+public class FileDemo {
+
+
+    public static void main(String[] args) {
+        String response = "";
+        File file = new File("D:\\code\\javaProject\\TC.WisdomTravel.Transfer.FrontStage.Union\\TransferplanGatewayMain\\src\\main\\resources\\env\\test\\sameTrain.txt");
+
+        try {
+            response = FileUtils.readFileToString(file, "utf-8");
+        } catch (Exception e) {
+        }
+    }
+
+
+}
